8fb975b204d4db6122da6a8cbab6a20bc25ca405
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-13 14:39:19 +0500

    added the comment

--- PR-test-project/index.js
+++ PR-test-project/index.js
@@ -3,6 +3,10 @@ import express from express
 const app = express();
 
 
+// app.listen(3001 , ()=>{
+//     app.listen(`app listening on port 3000`)
+// })
+
 app.listen(3001 , ()=>{
     app.listen(`app listening on port 3000`)
 })
