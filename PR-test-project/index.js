7b359a97d58adbedbfa4409a898a42c49606058b
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-13 15:32:23 +0500

    more issues added

--- PR-test-project/index.js
+++ PR-test-project/index.js
@@ -18,6 +18,14 @@ app.listen(3001 , ()=>{
 })
 
 
+const app = express();
+const app = express();
+
+app.listen(3001 , ()=>{
+    app.listen(`app listening on port 3000`)
+})
+
+
 app.listen(3001 , ()=>{
     app.listen(`app listening on port 3000`)
 })
