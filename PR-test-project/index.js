78efb5db97d947e7e1d2d36c89e7a6dc4e90c328
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-16 16:37:03 +0500

    removes some code

--- PR-test-project/index.js
+++ PR-test-project/index.js
@@ -8,22 +8,6 @@ const port = 3001;
 
 import express from "express";
 const app = express();
-import express from "express";
-const app = express();
-
-const port = 3001;
-
-
-
-
-import express from "express";
-const app = express();
-
-const port = 3001;
-
-
-
-
 
 const port = 3001;
 
